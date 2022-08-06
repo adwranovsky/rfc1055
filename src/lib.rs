b6949fe504b5e52f80c37f48c5d5588ae4913159
Alexander Wranovsky <alex@wranovsky.org>
2022-08-06 15:25:19 -0500

    Got started on encoder

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ pub const ESC_ESC: u8 = 0o335;
 pub enum DecodeError {
     BadEscape,
     ReadError,
+    ZeroLengthBuffer,
 }
 
 enum DecoderState {
@@ -35,6 +36,13 @@ pub struct Decoder<T>
 impl<T> Decoder<T>
     where T: FnMut() -> nb::Result<u8,()>
 {
+    ///
+    /// Create a new decoder using the provided reader.
+    ///
+    /// # Arguments
+    ///
+    /// * `reader` - A function that reads individual `u8`s from some input source that may block.
+    ///
     pub fn new(reader: T) -> Decoder<T> {
         Decoder {
             state: DecoderState::DiscardToEnd,
@@ -49,7 +57,25 @@ impl<T> Decoder<T>
     ///
     /// # Arguments
     ///
-    /// * `buf` - A u8 slice to write the decoded data into
+    /// * `buf` - A mutable u8 slice to write the decoded data into. It is not guaranteed that the
+    ///           whole packet will be written in one call.
+    ///
+    ///
+    /// # Results
+    ///
+    /// * `nb::Result::Err(nb::Error::WouldBlock)` is returned if there is no data to receive and
+    ///   there is no other problem.
+    /// * `nb::Result::Err(nb::Error::Other(rfc1055::DecodeError::ReadError))` is returned if the
+    ///   underlying reader function returned an error, and indicates any data previously read for
+    ///   this packet should be discarded by the caller. The caller should not rely on `buf` being
+    ///   unmodified.
+    /// * `nb::Result::Err(nb::Error::Other(rfc1055::DecodeError::ZeroLengthBuffer))` is returned
+    ///   if the decoder is ready to read more data, but the length of `buf` is zero.
+    /// * `nb::Result::Ok(num_read)` is returned if reading was successful, with `num_read`
+    ///   returning the total number of bytes read. `num_read` will never exceed the length of
+    ///   `buf`. A packet boundary is indicated with `num_read` equal to zero. Therefore, at least
+    ///   two calls to `read()` are needed for the caller to know that a complete packet has been
+    ///   read.
     ///
     ///
     /// # Examples
@@ -73,7 +99,6 @@ impl<T> Decoder<T>
     /// ```
     ///
     pub fn read(&mut self, buf: &mut [u8]) -> nb::Result<usize, DecodeError> {
-        let mut num_written: usize = 0;
 
         // Signal end-of-frame if needed
         if let DecoderState::End = self.state {
@@ -94,8 +119,14 @@ impl<T> Decoder<T>
             }
         }
 
+        // The decoder is ready to read more bytes, so signal an error if the buffer is empty
+        if buf.len() == 0 {
+            return Err(nb::Error::Other(DecodeError::ZeroLengthBuffer));
+        }
+
         // Decode bytes and write to the buffer until we either fill the buffer, need to block on
         // more input being available, or reach the end of the frame
+        let mut num_written: usize = 0;
         for item in buf.iter_mut() {
             // Loop until we find a valid character sequence
             loop {
@@ -177,6 +208,138 @@ pub fn decode_from_buffer(buffer: &[u8]) -> Decoder<impl FnMut() -> nb::Result<u
 
     Decoder::new(reader)
 }
+
+#[derive(PartialEq)]
+#[derive(Debug)]
+pub enum EncodeError {
+    BufferChanged,
+    WriteError,
+}
+
+enum EncoderState {
+    ReadFromBuf,
+    EscapeEscape,
+    EscapeEnd,
+    End,
+}
+
+pub struct Encoder<T>
+    where T: FnMut(u8) -> nb::Result<(),()>
+{
+    state: EncoderState,
+    writer: T,
+}
+
+impl<T> Encoder<T>
+    where T: FnMut(u8) -> nb::Result<(),()>
+{
+    pub fn new(writer: T) -> Encoder<T> {
+        Encoder {
+            state: EncoderState::End,
+            writer: writer,
+        }
+    }
+
+    ///
+    /// Writes to the encoder from the given buffer. It mirrors the `std::io::Write::write`
+    /// function as closely as possible, while adding support for `no_std` and the non-blocking
+    /// crate.
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// * `buf` - A `u8` slice to encode. The encoder might not write out the whole slice in one
+    ///    call.
+    ///
+    ///
+    /// # Results
+    ///
+    ///
+    pub fn write(&mut self, buf: &[u8]) -> nb::Result<usize, EncodeError> {
+        // Signal end-of-frame if needed
+        if let EncoderState::End = self.state {
+            // This state is only possible if buf.len() is equal to or less than one, so signal to
+            // the application that it made an error if we detect otherwise. The application can
+            // recover from this state by calling `write()` with buf.len() equal to zero.
+            if buf.len() > 1 {
+                return Err(nb::Error::Other(EncodeError::BufferChanged));
+            }
+
+            // Try to write the end-of-frame character
+            match (self.writer)(END) {
+                Ok(_) => {
+                    self.state = EncoderState::ReadFromBuf;
+                    return Ok(buf.len());
+                },
+                Err(nb::Error::WouldBlock) => {
+                    return Err(nb::Error::WouldBlock);
+                },
+                Err(nb::Error::Other()) => {
+                    return Err(nb::Error::Other(EncodeError::WriteError));
+                },
+            }
+        }
+
+        let mut num_read: usize = 0;
+        for item in buf {
+            // Some values require multiple symbols to be written, so loop until all needed symbols
+            // are written for this item
+            loop {
+                let (next_state, increment_counter, val_to_write) = match self.state {
+                    EncoderState::ReadFromBuf => {
+                        match item {
+                            END => (EncoderState::EscapeEnd, false, ESC),
+                            ESC => (EncoderState::EscapeEscape, false, ESC),
+                            x => (EncoderState::ReadFromBuf, true, x),
+                        }
+                    },
+                    EncoderState::EscapeEscape => {
+                        (EncoderState::ReadFromBuf, true, ESC_ESC)
+                    },
+                    EncoderState::EscapeEnd => {
+                        (EncoderState::ReadFromBuf, true, ESC_END)
+                    },
+                    EncoderState::End, => {
+                        (EncoderState::ReadFromBuf, true, END)
+                    },
+                };
+            }
+
+            match (self.writer)(val_to_write) {
+                Ok(_) => {
+                    
+                },
+                Err(nb::Error::WouldBlock) => {
+                    if num_read > 0 {
+                        return Ok(num_read);
+                    } else {
+                        return Err(nb::Error::WouldBlock);
+                    }
+                },
+                Err(nb::Error::Other(_)) => {
+                    return Err(nb::Error::Other(EncodeError::WriteError));
+                },
+            }
+        }
+
+        // Write end-of-frame
+        match (self.writer)(END) {
+            Ok(_) => {
+                self.state = EncoderState::ReadFromBuf;
+                return Ok(buf.len());
+            },
+            Err(nb::Error::WouldBlock) => {
+                self.state = EncoderState::End;
+                return Ok(num_read);
+            },
+            Err(nb::Error::Other()) => {
+                self.state = EncoderState::ReadFromBuf;
+                return Err(nb::Error::Other(EncodeError::WriteError));
+            },
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
